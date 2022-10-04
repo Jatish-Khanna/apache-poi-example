dee9b3b7c620da4e0861731a0ddd3ced98d981ba
Gauravkumar29 <gaurav.kumarsd29@gmail.com>
2022-10-04 09:36:06 +0530

    Add files via upload

---
+++ src/main/java/com/example/poiexample/PoiexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.poiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoiexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/poiexample/PoiexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
