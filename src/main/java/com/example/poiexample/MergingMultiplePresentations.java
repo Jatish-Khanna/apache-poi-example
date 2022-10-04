2216e5df42001a9f72b593759c76ec3b3d45622d
Jatish Khanna <jatish.khanna@gmail.com>
2022-10-04 09:53:27 +0530

    Update MergingMultiplePresentations.java

--- src/main/java/com/example/poiexample/MergingMultiplePresentations.java
+++ src/main/java/com/example/poiexample/MergingMultiplePresentations.java
@@ -1,1 +1,24 @@
+package com.example.poiexample;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MergingMultiplePresentations {
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void merge() throws IOException {
+
+		// creating empty presentation
+		try (XMLSlideShow ppt = new XMLSlideShow()) {
+
+			// taking the two presentations that are to be merged
+			String file1 = "presentation1.pptx";
+			String file2 = "presentation2.pptx";
