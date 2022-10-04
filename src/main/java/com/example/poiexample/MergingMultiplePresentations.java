fafeba5d4114864296d39f831f9e658a5b65b0b3
Jatish Khanna <jatish.khanna@gmail.com>
2022-10-04 09:53:45 +0530

    Update MergingMultiplePresentations.java

--- src/main/java/com/example/poiexample/MergingMultiplePresentations.java
+++ src/main/java/com/example/poiexample/MergingMultiplePresentations.java
@@ -22,3 +22,29 @@ public class MergingMultiplePresentations {
 			// taking the two presentations that are to be merged
 			String file1 = "presentation1.pptx";
 			String file2 = "presentation2.pptx";
+			String[] inputs = { file1, file2 };
+
+			for (String arg : inputs) {
+
+				FileInputStream inputstream = new FileInputStream(arg);
+				try (XMLSlideShow src = new XMLSlideShow(inputstream)) {
+
+					for (XSLFSlide srcSlide : src.getSlides()) {
+
+						// merging the contents
+						ppt.createSlide(srcSlide.getSlideLayout());
+					}
+				}
+			}
+
+			String file3 = "combinedpresentation.pptx";
+
+			// creating the file object
+			FileOutputStream out = new FileOutputStream(file3);
+
+			// saving the changes to a file
+			ppt.write(out);
+			System.out.println("Merging done successfully");
+			out.close();
+		}
+	}
